8a0090a5e51a0053100dc1109e10f813cd17f2fd
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2017-06-19 09:17:28 -0600

    Remove direct recursion from SchemaAlg to allow annotation of the tree.

--- src/Data/Schema.purs
+++ src/Data/Schema.purs
@@ -2,25 +2,41 @@ module Main where
 
 import Control.Applicative.Free (FreeAp)
 import Data.Exists (Exists)
+import Data.Lazy (Lazy, force)
 import Data.Leibniz (type (~))
 import Data.Map (Map)
 import Data.Maybe (Maybe)
 
-data SchemaAlg p a 
+data HCofree (g :: (Type -> Type) -> Type -> Type) (a :: Type) (i :: Type)= HCofree a (Lazy (g (HCofree g a) i))
+
+head :: forall g a i. HCofree g a i -> a 
+head (HCofree h _) = h
+
+tail :: forall g a i. HCofree g a i -> g (HCofree g a) i
+tail (HCofree _ t) = force t
+
+newtype Schema (p :: Type -> Type) (a :: Type) (i :: Type) = Schema { runSchema :: HCofree (SchemaAlg p) a i }
+
+data SchemaAlg (p :: Type -> Type) (g :: Type -> Type) (a :: Type) 
   = Prim (p a)
-  | Rec  (FreeAp (PropSchema a p) a)
-  | Vect (Exists (VectElem p a))
-  | Sum  (Array  (Exists (Constr p a)))
-  | Dict (Exists (DictElem p a))
+  | Rec  (FreeAp (PropSchema g a) a)
+  | Sum  (Array  (Exists (Constr g a)))
+  | Vect (Exists (VectElem g a))
+  | Dict (Exists (DictElem g a))
 
-data VectElem p a b = VectElem (SchemaAlg p b) (a ~ Array b)
-data DictElem p a b = DictElem (SchemaAlg p b) (a ~ Map String b)
+data PropSchema (g :: Type -> Type) (o :: Type) (a :: Type)
+  = Required (Req g o a)
+  | Optional (Exists (Opt g o a))
 
-data PropSchema o p a 
-  = Required (Req o p a)
-  | Optional (Exists (Opt o p a))
+data Req (g :: Type -> Type) (o :: Type) (a :: Type) 
+  = Req { id :: String, valueSchema :: g a, accessor :: o -> a, dflt :: Maybe a } 
 
-data Req o p a   = Req { id :: String, valueSchema :: SchemaAlg p a, accessor :: o -> a, dflt :: Maybe a } 
-data Opt o p a b = Opt { id :: String, valueSchema :: SchemaAlg p b, accessor :: o -> Maybe b } (a ~ Maybe b) 
+data Opt (g :: Type -> Type) (o :: Type) (a :: Type) (b :: Type) 
+  = Opt { id :: String, valueSchema :: g b, accessor :: o -> Maybe b } (a ~ Maybe b) 
 
-data Constr p a b = Constr { id :: String, baseSchema :: SchemaAlg p b, f :: b -> a, g :: a -> Maybe b }
+data Constr (g :: Type -> Type) (a :: Type) (b :: Type) 
+  = Constr { id :: String, baseSchema :: g b, g :: b -> a, g :: a -> Maybe b }
+
+data VectElem (g :: Type -> Type) (a :: Type) (b :: Type) = VectElem (g b) (a ~ Array b)
+data DictElem (g :: Type -> Type) (a :: Type) (b :: Type) = DictElem (g b) (a ~ Map String b)
+
